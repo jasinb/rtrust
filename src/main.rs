4f4c0595eb287e2ce403c9c2563db5d38ff9339c
Jasin Bushnaief <jasinb@jasinb.com>
2023-09-20 19:38:39 +0300

    First commit. Sphere normals

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+
+use vec3::unit_vector;
+
+use crate::vec3::{Vec3, dot};
+use crate::ray::Ray;
+
+mod vec3;
+mod ray;
+
+fn hit_sphere(center: &Vec3, radius: f32, r: &Ray) -> f32 {
+    let oc = r.orig - *center;
+    let a = dot(&r.dir, &r.dir);
+    let b = 2.0 * dot(&oc, &r.dir);
+    let c = dot(&oc, &oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    return if discriminant >= 0.0 { (-b - discriminant.sqrt()) / (2.0 * a) } else { -1.0 };
+}
+
+fn ray_color(ray: &Ray) -> Vec3 {
+    let t = hit_sphere(&Vec3(0.0, 0.0, -1.0), -0.5, ray);
+    
+    if t > 0.0 {
+        let n = unit_vector(&(ray.at(t) - Vec3(0.0, 0.0, -1.0)));
+        return 0.5 * (n + Vec3(1.0, 1.0, 1.0));
+    }
+
+    let unit_dir = vec3::unit_vector(&ray.dir);
+    let a = 0.5 * unit_dir.1 + 1.0;
+    return (1.0 - a) * Vec3(1.0, 1.0, 1.0) + a * Vec3(0.5, 0.7, 1.0)
+}
+
+
+fn main() {
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = ((image_width as f32) / aspect_ratio) as i32;
+    
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f32) / image_height as f32;
+    let camera_center = Vec3(0.0, 0.0, 0.0);
+    let viewport_u = Vec3(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3(0.0, -viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / image_width as f32;
+    let pixel_delta_v = viewport_v / image_height as f32;
+
+    let viewport_upper_left = camera_center - Vec3(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let offs = pixel_delta_u + pixel_delta_v;
+    let half_offs = 0.5 * offs;
+    let pixel00_loc = viewport_upper_left +  half_offs;
+    
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() > 1);
+    let mut f = File::create(&args[1]).unwrap();
+
+    write!(&mut f, "P3\n{image_width} {image_height}\n255\n").unwrap();
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let pixel_center = pixel00_loc + ((i as f32) * pixel_delta_u) + ((j as f32) * pixel_delta_v);
+            let ray_dir = pixel_center - camera_center;
+            let ray = Ray{orig: camera_center, dir: ray_dir};
+
+            ray_color(&ray).write_color(&mut f);
+        }
+    }
+}
---
+++ src/vec3.rs
@@ -0,0 +1,79 @@
+use std::fs::File;
+use std::io::Write;
+use std::ops::{Add, Sub, Mul, Div, Neg};
+
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vec3(pub f32, pub f32, pub f32);
+
+impl Mul<f32> for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: f32) -> Self {
+        Self(self.0 * rhs, self.1 * rhs, self.2 * rhs)
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        rhs * self
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Self;
+    fn div(self, rhs: f32) -> Self {
+        self * (1.0 / rhs)
+    }
+}
+
+impl Add<Self> for Vec3 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Self(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self(-self.0, -self.1, -self.2)
+    }
+}
+
+impl Sub<Self> for Vec3 {
+    type Output = Self;
+    fn sub(self, rhs: Vec3) -> Self {
+        self + (-rhs)
+    }
+}
+
+impl Vec3 {
+    pub fn write_color(self, f: &mut File) {
+        let Self(r, g, b)  = self * 255.999 as f32;
+        
+        let r = r as i32;
+        let g = g as i32;
+        let b = b as i32;
+
+        write!(f, "{r} {g} {b}\n").unwrap();
+    }
+
+    pub fn length_squared(&self) -> f32 {
+        dot(self, self)
+    }
+
+    pub fn length(&self) -> f32 {
+        self.length_squared().sqrt()
+    }
+}
+
+pub fn unit_vector(v: &Vec3) -> Vec3 {
+    *v / v.length()
+}
+
+pub fn dot(a: &Vec3, b: &Vec3) -> f32 {
+    a.0*b.0 + a.1*b.1 + a.2*b.2
+}
+
+
